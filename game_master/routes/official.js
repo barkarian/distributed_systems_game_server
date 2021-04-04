1123f5366fe52a7bd5106d604e76ce5a4c57544e
barkarian <thodorisbarkas@gmail.com>
2021-04-04 22:20:57 +0300

    create-tournament route AND new utils file :createTuples(a module) AND creating two 3 tables:tournaments,tournament_winners and most importantly tournament_games

--- game_master/routes/official.js
+++ game_master/routes/official.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authorizationOfficial = require("../middleware/authorizationOfficial");
 const pool = require("../db");
+const { startTournament } = require("../utils/createTuples");
+const e = require("express");
 //const { tournamentMatchesRelation } = require("../utils/createTuples");
 
 router.get("/get-all-players", authorizationOfficial, async (req, res) => {
@@ -20,9 +22,34 @@ router.get("/get-all-players", authorizationOfficial, async (req, res) => {
 
 router.post("/create-tournament", authorizationOfficial, async (req, res) => {
   try {
-    const gameBacket = req.body;
-    //tournamentMatchesRelation();
-    res.json(gameBacket);
+    //1.check if players are enough
+    const { tourn_users, tournament_name } = req.body;
+    if (tourn_users.length <= 4) {
+      res.status(500).json("You need more users for creating a tournament");
+    }
+    //2.creating the gameBacket
+    var gameBacket = tourn_users;
+    //3.calculate tournament phases
+    var phases = Math.log2(gameBacket.length);
+    phases = Math.floor(phases) - 1;
+    //4.create tournament tuple
+    const tournament = await pool.query(
+      `INSERT INTO tournaments(tournament_name,total_players) VALUES ('${tournament_name}',${gameBacket.length}) RETURNING *`
+    );
+    if (tournament.rows.length == 0) {
+      throw "Something went wrong tournament hasn't created";
+    }
+    //fetch the data to database tournament_games table
+    const success = await startTournament(
+      gameBacket,
+      phases,
+      tournament.rows[0].tournament_id
+    );
+    if (success) {
+      res.json(tournament.rows[0].tournament_id);
+    } else {
+      res.status(500).json("Couldn't start tournament");
+    }
   } catch (err) {
     console.error(err.message);
     res.status(500).json("Server Error");
--- server/database.sql
+++ server/database.sql
@@ -15,5 +15,38 @@ CREATE TABLE users(
 
 --insert fake users
 
-INSERT INTO users(user_name,user_email,user_password) VALUES ('Teo','teo@gmail.com','teopass');
+INSERT INTO users(user_name,user_email,user_password) VALUES ('admin','admin@gmail.com','password');
 UPDATE users SET user_role_admin='1' WHERE user_name='admin'
+
+CREATE TABLE tournament_games(
+    game_id uuid DEFAULT NULL,
+    running BIT,
+    scheduled BIT,
+    endgame BIT,
+    finished BIT,
+    wait_match1 uuid DEFAULT NULL, --IS NULL if phase is 1
+    wait_match2 uuid DEFAULT NULL, --IS NULL if phase is 1
+    player1 uuid DEFAULT NULL, --is NULL for phase >=2 except *sometimes* for phase_id=1 and phase=2
+    player2 uuid DEFAULT NULL, --is NULL for phase >=2
+    phases int, 
+    phase int,
+    phase_id int,
+    tournament_id uuid,
+    tournament_game_id uuid PRIMARY KEY 
+);
+
+CREATE TABLE tournaments(
+    tournament_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    tournament_name VARCHAR(255) NOT NULL,
+    finished BIT DEFAULT '0',
+    total_players int
+);
+
+CREATE TABLE tournament_winners(
+    tournament_id uuid NOT NULL,
+    winner_id uuid NOT NULL,
+    PRIMARY KEY(tournament_id,winner_id)
+);
+
+INSERT INTO tournaments(tournament_name,total_players) VALUES ('awesome tournament name',15);
+
